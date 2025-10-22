fb2fcb694f6ec998e5644a299e8d1f4e164365b9
royroyalle <21ke21i3387@21kschool.school>
2025-10-22 20:38:00 +0530

    ..

---
+++ Lesson 4/Activity1.py
@@ -0,0 +1,10 @@
+tree1 = 54
+tree2 = 45
+tree3 = 89
+tree4 = 14
+tree5 = 90 
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5 
+print("The sum of all tree heights are-", sum)
+average = sum/5
+print("The Average needed height of your desired tree is-", average)
