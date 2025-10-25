687693a11fbfb1b022a758aae661150f5d6dd3f6
royroyalle <21ke21i3387@21kschool.school>
2025-10-25 19:28:02 +0530

    ..

---
+++ Lesson 5/Activity1.py
@@ -0,0 +1,7 @@
+n = 4
+if n > 0:
+    print ("The number", n, "is positive")
+
+num = -6
+if num < 0:
+    print("The number", num,"is negative")
---
+++ Lesson 5/Activity3.py
@@ -0,0 +1,5 @@
+num = int(input("Please enter a number"))
+if num > 15:
+    print(num,"is greater than 15")
+else:
+    print(num,"is lesser than 15")
