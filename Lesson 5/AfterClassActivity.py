76a6008ae7fdebf76e32f23ca4fdd7325f058096
royroyalle <21ke21i3387@21kschool.school>
2025-10-26 16:23:11 +0530

    .

---
+++ Lesson 5/AfterClassActivity.py
@@ -0,0 +1,6 @@
+print("Welcome to the clothing suggester")
+temp = int(input("Please enter the current temperature of the day"))
+if temp < 26:
+    print("Kindly wear a sweat shirt or hoodie")
+else:
+    print("Kindly wear a T-shirt or sleeveless or a shirt")
