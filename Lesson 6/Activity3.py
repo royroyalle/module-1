ab84ce71b16be87ee4af0e6d601da8bef626a9c2
royroyalle <21ke21i3387@21kschool.school>
2025-10-26 19:31:01 +0530

    .

---
+++ Lesson 6/Activity3.py
@@ -0,0 +1,17 @@
+q = 12
+w = 15
+e = 3
+print(q!=w)
+print(w!=e)
+
+q = "Python"
+w = "Coding"
+
+if q!=w:
+    print( q, "and", w, "are not equal")
+
+
+q = 4
+w = 5
+if (q==1) != (w==5):
+    print("Hello")
