8f1b109adba34f91e7dad24f8cf80fed553c57bc
royroyalle <21ke21i3387@21kschool.school>
2025-11-04 08:54:43 +0530

    .

---
+++ Lesson 8/AfterClassProject.py
@@ -0,0 +1,13 @@
+x = int(input("\nPlease enter the interchanged value of x-"))
+y = int(input("\nPlease enter the interchanged value of y-"))
+z = int(input("\nPlease enter the interchanged value of z-"))
+x1 = int(input("\nPlease enter the true value of x-"))
+y1 = int(input("\nPlease enter the true value of y-"))
+z1 = int(input("\nPlease enter the true value of z-"))
+x = x1
+y = y1
+z = z1
+print ("The correct values now are-\n")
+print(x,"\n")
+print(y,"\n")
+print(z,"\n")
